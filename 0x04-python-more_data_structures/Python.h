ebc9614014c431995f8f6b8b71f113de17c68102
Akubada <dankubada@gmail.com>
2024-01-10 09:05:01 -0800

    Data Structure and more

---
+++ 0x04-python-more_data_structures/Python.h
@@ -0,0 +1,26 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+square_matrix_simple(matrix=[]):
+search_replace(my_list, search, replace):
+best_score(a_dictionary):
+weight_average(my_list=[]):
+square_matrix_map(matrix=[]):
+complex_delete(a_dictionary, value):
+print_python_bytes(PyObject *p)
+PyBytesObject *bytes = (PyBytesObject *) p;
+print_python_list(PyObject *p)
+PyListObject *lst = (PyListObject *) p;
+mutiply_list_map(my_list=[], number=0):
+roman_to_int(roman_string):
+uniq_add(my_list=[]):
+common_elements(set_1, set_2):
+only_diff_elements(set_1, set_2):
+number_keys(a_dictionary):
+print_sorted_dictionary(a_dictionary):
+update_dictionary(a_dictionary, key, value):
+simple_delete(a_dictionary, key=""):
+multiply_by_2(a_dictionary):
+
+#endif /* PYTHON_H */
+
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+This file belongs to 0x04-python-more_data_structures
