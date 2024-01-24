2892b60ee270c7e31c2ed83923d8d5692b14f22e
Akubada <dankubada@gmail.com>
2024-01-23 17:31:36 -0800

    This project

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-    except ValueError:
-        return False
-    return True
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+        else:
+            return False
+        except ValueError:
+            raise ValueError
